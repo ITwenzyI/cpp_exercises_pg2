1688584651b81dc37cfa1908a7ed33e16300ffe7
ItwenzyI <kischoenhaar@gmail.com>
2025-06-16 00:26:50 +0200

    Added first Exercise

---
+++ medizinische-notizen-fstream.cpp
@@ -0,0 +1,15 @@
+/*
+ * File: medizinische
+ * Author: Kilian | ItwenzyI
+ * Created: 16.06.2025
+ * Description: Stell dir vor, du entwickelst eine kleine Funktion für ein medizinisches Assistenzprogramm.
+ * In diesem Programm sollen Tagesnotizen eines Arztes gespeichert und bei Bedarf ausgelesen werden können.
+ */
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
